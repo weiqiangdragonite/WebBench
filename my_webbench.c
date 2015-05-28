453ef9301dfbe986652da385f3a66158b57b69e1
Dragonite <weiqiangdragonite@gmail.com>
2015-05-28 22:59:53 +0800

    添加了build_request函数

--- my_webbench.c
+++ my_webbench.c
@@ -12,6 +12,8 @@
 #include <string.h>
 #include <getopt.h>
 
+#include <netdb.h>
+
 /* Marco */
 #define PROGRAM_VERSION	"1.5"
 
@@ -20,6 +22,7 @@
 #define METHOD_HEAD	1
 #define METHOD_OPTIONS	2
 #define METHOD_TRACE	3
+
 
 /* Global variables */
 int force = 0;		/* ?? */
@@ -30,6 +33,10 @@ int clients = 1;	/* run n http clients, default n = 1 */
 int method = METHOD_GET;	/* HTTP method */
 char *proxy_host = NULL;
 int proxy_port = 80;
+
+char host[NI_MAXHOST];
+#define REQUEST_SIZE	2048
+char request[REQUEST_SIZE];
 
 
 /* long options structure */
@@ -54,6 +61,7 @@ static const struct option long_options[] = {
 
 /* Function prototypes */
 void usage(void);
+void build_request(const char *url);
 
 
 /*
@@ -114,8 +122,6 @@ main(int argc, char *argv[])
 		case 'p':
 			/* proxy server parsing - "server:port" */
 			/* strrchr() - find character from the end */
-			/* still cannot check come bad parameters, such
-			xxx:xxx:xxx:ddd */
 			tmp = strrchr(optarg, ':');
 			proxy_host = optarg;
 			if (tmp == NULL)
@@ -169,6 +175,8 @@ main(int argc, char *argv[])
 	fprintf(stderr, "WebBench - Simple Web Benchmark "PROGRAM_VERSION"\n"
 		"Copyright (c)  Radim Kolar 1997-2004, GPL Open Source Software.\n");
 
+	build_request(argv[optind]);
+
 	return 0;
 }
 
@@ -194,4 +202,125 @@ usage(void)
 		"  --trace                  Use TRACE request method.\n"
 		"  -h|--help                This information.\n"
 		"  -v|--version             Display program version.\n");
+}
+
+/*
+ * build the reuqest header, such:
+ * GET /abc HTTP/1.0\r\n
+ * Connection: close\r\n
+ * \r\n
+ */
+void
+build_request(const char *url)
+{
+	char tmp[10];
+	int i;
+
+	memset(request, 0, REQUEST_SIZE);
+	memset(host, 0, NI_MAXHOST);
+
+	/* I don't understand */
+	if (force_reload && proxy_host != NULL && http_ver < 1)
+		http_ver = 1;
+
+	if (method == METHOD_HEAD && http_ver < 1)
+		http_ver = 1;
+
+	if (method == METHOD_OPTIONS && http_ver < 2)
+		http_ver = 2;
+
+	if (method == METHOD_TRACE && http_ver < 2)
+		http_ver = 2;
+
+
+	switch (method) {
+	case METHOD_HEAD:
+		strncpy(request, "GET", REQUEST_SIZE);
+		break;
+	case METHOD_OPTIONS:
+		strncpy(request, "OPTIONS", REQUEST_SIZE);
+		break;
+	case METHOD_TRACE:
+		strncpy(request, "TRACE", REQUEST_SIZE);
+		break;
+	case METHOD_GET:
+	default:
+		strncpy(request, "GET", REQUEST_SIZE);
+		break;
+	}
+
+	strcat(request, " ");
+
+	if (strstr(url, "://") == NULL) {
+		fprintf(stderr, "\n%s: is not a valid URL.\n", url);
+		exit(1);
+	}
+	if (strlen(url) > 1500) {
+		fprintf(stderr, "URL is too long.\n");
+		exit(1);
+	}
+
+	if (proxy_host == NULL) {
+		/* compare two strings ignoring case */
+		if (strncasecmp("http://", url, 7) != 0) {
+			fprintf(stderr, "\nOnly HTTP protocol is directly "
+				"supported, set --proxy for others.\n");
+			exit(1);
+		}
+	}
+
+	/* protocol/host delimiter(定界符)*/
+	/* If url is "http://abc.com/", then i = 7 */
+	i = strstr(url, "://") - url + 3;
+
+	if (strchr(url+i, '/') == NULL) {
+		fprintf(stderr, "\nInvalid URL syntax - hostname must ends with '/'");
+		exit(1);
+	}
+
+	if (proxy_host == NULL) {
+		/* get host and port from hostname */
+		/* if url is "http://abc.com:10086/test", then
+		host = "abc.com", port = 10086, request = GET /test */
+		if (index(url+i, ':') != NULL &&
+		(index(url+i, ':') < index(url+i, '/'))) {
+			strncpy(host, url+i, strchr(url+i, ':') - url - i);
+			memset(tmp, 0, 10);
+			strncpy(tmp, index(url+i, ':') + 1,
+				strchr(url+i, '/') - index(url+i, ':') - 1);
+			proxy_port = atoi(tmp);
+		} else {
+			/* strcspn - get length of a prefix substring */
+			strncpy(host, url + i, strcspn(url+i, "/"));
+		}
+		/* printf("host = %s, port = %d\n", host, proxy_port); */
+		/* printf("strcspn(url+i, /) = %d\n", (int) strcspn(url+i, "/")); */
+		strcat(request + strlen(request), url + i + strcspn(url+i, "/"));
+	} else {
+		/* why? I don't understand proxy */
+		strcat(request, url);
+	}
+	printf("rqeuest = %s\n", request);
+
+	if (http_ver == 1)
+		strcat(request, " HTTP/1.0");
+	else if (http_ver == 2)
+		strcat(request, " HTTP/1.1");
+	strcat(request, "\r\n");
+
+	if (proxy_host == NULL && http_ver > 0) {
+		strcat(request, "Host: ");
+		strcat(request, host);
+		strcat(request, "\r\n");
+	}
+
+	if (force_reload && proxy_host != NULL)
+		strcat(request, "Pragma: no-cache\r\n");
+
+	/* HTTP/1.1 default is keep-alive */
+	if (http_ver > 1)
+		strcat(request, "Connection: close\r\n");
+
+	if (http_ver > 0)
+		strcat(request, "\r\n");
 }
